e9d20ac842ccd77b5eb5f1a3a64884042bc5dff0
victoria <vjohkv@gmail.com>
2019-05-03 15:06:10 -0700

    Remove debugger from login

--- frontend/components/SessionForm.jsx
+++ frontend/components/SessionForm.jsx
@@ -33,7 +33,6 @@ class SessionForm extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
         const user = Object.assign({}, this.state);
-        debugger
         this.props.processForm(user)
             .then(() => this.props.history.push('/profile'));
     }
