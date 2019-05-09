22061679785d5122c3ae9edb54c157021b961465
victoria <vjohkv@gmail.com>
2019-05-09 10:24:19 -0700

    Fix post create to not require refresh + fix heroku! (credits to Aiden)

--- frontend/components/Feed/FeedIndexItem.jsx
+++ frontend/components/Feed/FeedIndexItem.jsx
@@ -5,6 +5,9 @@ import { withRouter } from 'react-router';
 class FeedIndexItem extends React.Component {
     constructor({props}) {
         super(props);
+        this.state = {
+            author_id: 0
+        };
 
     }
     // let date = post.created_at;
@@ -16,15 +19,19 @@ class FeedIndexItem extends React.Component {
         // this.props.deletePost(this.props.currentUser)
 
     }
-    
+
     render() {
+        
     return (
+        
         <li className="posts">
             <div className="options"><a href="#">...</a></div>
             <div className="post-author">
                 <img className="profile-pic" src="https://2.bp.blogspot.com/-VkDNVsRGYXU/XNB-EKNKlSI/AAAAAAAABUA/A3SKtQ-hyUUcAsuOrfISYksxvPEcmO8JQCKgBGAs/s1600/2.png"/>
                 <h5>
-                    <a href={`#/users/${this.props.post.author.id}`}>{`${this.props.post.author.first_name} ${this.props.post.author.last_name}`}</a>
+                    {!(this.props.post.author == undefined) ? <a href={`#/users/${this.props.post.author.id}`}>{`${this.props.post.author.first_name} ${this.props.post.author.last_name}`}</a>
+                    : <a href={`#/users/${this.props.user.id}`}>{`${this.props.user.first_name} ${this.props.user.last_name}`}</a>} 
+                    
                     <span className="date">May 7, 2018</span>
                 </h5>
             </div>
@@ -39,9 +46,6 @@ class FeedIndexItem extends React.Component {
                 <span>Comment</span>
                 <span>Share</span>
             </div>
-
-            {/* <div className="linebreak">
-            </div> */}
 
             <form className="comment">
                 <img className="comment-placeholder" src="https://2.bp.blogspot.com/-VkDNVsRGYXU/XNB-EKNKlSI/AAAAAAAABUA/A3SKtQ-hyUUcAsuOrfISYksxvPEcmO8JQCKgBGAs/s1600/2.png"/>
