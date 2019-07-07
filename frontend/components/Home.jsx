91a48186ca4c297938ffc50bb45069c3188a08f6
victoria <vjohkv@gmail.com>
2019-07-06 17:47:33 -0700

    Create icons for welcome page

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -6,9 +6,22 @@ import HomeFooter from './HomeFooter';
 class Home extends React.Component {
     render () { 
         return (
-        <div id="home_global">
+        <div className="home_global">
             <LoginContainer /> 
-            <SignupContainer />
+            <div className='homeContentContainer'>
+                <div className='introContainer'>
+                    <h1 className='introHeader'>Connect with friends and the world around you on Facebook.</h1>
+                    <ul className='welcomeList'>
+                        <li><img src='/images/welcome_news.png' />
+                            <p><span className='welcome_bold'>See photos and updates </span>from friends in News Feed.</p></li>
+                        <li><img src='/images/welcome_share.png' />
+                            <p><span className='welcome_bold'>Share what's new </span>in your life on your Timeline.</p></li>
+                        <li><img src='/images/welcome_more.png' />
+                            <p><span className='welcome_bold'>Find more </span>of what you're looking for with Facebook Search.</p></li>
+                    </ul>
+                </div>
+                <SignupContainer />
+            </div>
             <HomeFooter />
         </div>
         )
--- frontend/components/SignupForm.jsx
+++ frontend/components/SignupForm.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import {withRouter} from 'react-router';
 
 class SignupForm extends React.Component {
