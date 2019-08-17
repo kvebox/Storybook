c54cfeecbdd7e6b9667cb31ce8dc6a0bae50ced2
victoria <vjohkv@gmail.com>
2019-08-16 20:45:13 -0700

    Add icons for bio section

--- frontend/components/Widgets/BioWidget.jsx
+++ frontend/components/Widgets/BioWidget.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 
-export default () => {
-    return (
-        <div className="bio-widget">
-            <div className='bio-header-container'>
-                <img className='widget-header-icon' src='/images/widget_event.png'/>
-                <span className='bio-header'> Intro </span>
+class BioWidget extends React.Component{
+
+    bioInfo(){
+        return(
+        <p className='bio-info'>Add a short bio to tell people more about yourself.</p>
+        )
+    }
+
+    render() {
+        return (
+            <div className="bio-widget">
+                <div className='bio-header-container'>
+                    <img className='widget-header-icon' src='/images/widget_event.png'/>
+                    <span className='bio-header'> Intro </span>
+                </div>
+
+                <div className='bio-content-container'>
+                    <img className='bio-header-icon' src='/images/widget_bio.png'/>
+                    <div className='bio-info'>Add a short bio to tell people more about yourself.</div>
+                    <a className='add-bio-button'>Add Bio</a>
+                </div>
+
+                <div className='bio-follow-container'>
+                    <img className='bio-follow-icon' src='/images/widget_follow.png'/>
+                    <div className='bio-follow-text'>Follwed by 0 people</div>
+                </div>
+
+                <div className='bio-showcase-container'>
+                    <img className='bio-showcase-icon' src='/images/widget_showcase.png' />
+                    <div className='bio-info'>Showcase what's important to you by adding photos, pages, groups and more to your featured section on your public profile.</div>
+                    <a className='add-bio-button'>Add to Featured</a>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
+
+export default BioWidget;
